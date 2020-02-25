d11357d7c7435d3c589095c48a9da1c18fbea7a1
Nikolaos Pappas <npappas@uth.gr>
2020-02-25 21:32:21 +0200

    Remove lines

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 const partialsPath = path.join(__dirname, 'partials')
